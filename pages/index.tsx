6fc894b5f4acca82150472a4ba642973de641656
khalefa <khalefa@alahmad.org>
2023-07-28 07:15:01 +0000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import ChatDisplay from "@/components/ChatDisplay";
+import Hero from "@/components/Hero";
+import { useChatStore } from "@/stores/ChatStore";
+
+export default function Home() {
+  const apiKey = useChatStore((state) => state.apiKey);
+
+  // return apiKey ? <ChatDisplay /> : <Hero />;
+  return <ChatDisplay />;
+}
